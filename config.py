ea0ad428df7126410a70776e86f7180abb69021d
Sumit-1S <sumitsingh130801@gmail.com>
2025-04-06 23:59:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-BACKEND_RECOMMEND = "http://localhost:8000/recommend"
+BACKEND_RECOMMEND = "https://assessment-recommeder-backend.onrender.com/recommend"
 PATH_TESTS_INFO = "shl_assessments_full.csv"
 RECOMMENDATION_FIELDS = ["Assessment Name", "Test Type", "Remote Testing Support","Adaptive/IRT Support", "Assessment Length", "URL"]
 BASE_COURSE_URL = "https://shl.com"
